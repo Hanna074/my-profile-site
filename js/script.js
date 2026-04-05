0bdf02de521038af007c85fbccc73c27382121bc
최한나 <chn07120@naver.com>
2026-04-05 23:26:41 +0900

    인사말 메시지 수정

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
 function showMessage() {
-  document.getElementById("message").innerText = "반갑습니다! 방문해주셔서 감사합니다.";
+  document.getElementById("message").innerText = "안녕하세요! 제 페이지에 방문해주셔서 감사합니다.";
 }
